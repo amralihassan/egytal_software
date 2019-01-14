21f64d227e4dfbab88fae6290c0d8da7b48cc535
amralihassan <amr_dotnet@ymil.com>
2019-01-14 13:08:40 +0200

    galss update code

--- application/views/glass/glass_calc_view.php
+++ application/views/glass/glass_calc_view.php
@@ -4,6 +4,10 @@
 	    <li><a href="#" onclick="loadCutomersituation();"><i class="fa fa-dashboard"></i> الرئيسية</a>&nbsp;&nbsp;</li>
 	    <li class="active">حساب الزجاج</li>
 	  </ol>
+		<form id="frm_update" action="" method="post">
+			<div class="col-md-10" id="id_pro">
+			</div>
+		</form>
 	  <div class="row">
 	  	<div class="col-md-3">
 	  		<label><b>نوع قطعة الزجاج</b></label>
@@ -17,25 +21,25 @@
 	  <br>
 	  <div id="single" style="display: none;">
 		  <form action="" method="POST" id="frmSingle">
-			  <div class="row">	
+			  <div class="row">
 				  	<div class="col-md-3">
 				  		<label><b>اسم الزجاج</b></label>
 				  		<select class="form-control select2" style="width: 100%;" name="" id="">
 				  			<option value="">نوع الزجاج</option>
-				  		</select>	  		
+				  		</select>
+				  	</div>
+				  	<div class="col-md-2">
+				  		<label><b>العرض</b></label>
+				  		<input type="number" step="1" min="20" max="1000" name="" class="form-control">
+				  	</div>
+				  	<div class="col-md-2">
+				  		<label><b>اﻻرتفاع</b></label>
+				  		<input type="number" name="" step="20" min="1" max="1000" class="form-control">
 				  	</div>
 				  	<div class="col-md-1">
-				  		<label><b>العرض</b></label>
-				  		<input type="text" name="" class="form-control">
-				  	</div>	
-				  	<div class="col-md-1">
-				  		<label><b>اﻻرتفاع</b></label>
-				  		<input type="text" name="" class="form-control">
-				  	</div>		
-				  	<div class="col-md-1">
 				  		<label><b>العدد</b></label>
-				  		<input type="text" name="" class="form-control">
-				  	</div>	  			  			  				  	
+				  		<input type="number" name="" step="1" min="1" max="1000000" class="form-control">
+				  	</div>
 				  	<div class="col-md-2">
 				  		<label><b>سعر الزجاج</b></label>
 				  		<input type="text" name="" class="form-control" readonly="">
@@ -43,51 +47,51 @@
 				  	<div class="col-md-2">
 				  		<label><b>اﻻجمالي</b></label>
 				  		<input type="text" name="" class="form-control" readonly="">
-				  	</div>	  	
-			  </div>	  	
+				  	</div>
+			  </div>
 		  </form>
 		  <br>
-		  <button class="btn btn-success" name="">اضافة</button>	  	
+		  <button class="btn btn-success" name="">اضافة</button>
 		  <button class="btn btn-info">طباعة</button>
 	  </div>
-	  <div id="double" style="display: none;"> 
+	  <div id="double" style="display: none;">
 		  	<form action="" method="POST" id="frmDouble">
 			  <div class="row">
 				  	<div class="col-md-3">
 				  		<label><b>اسم الزجاج الداخلي</b></label>
 				  		<select class="form-control select2" style="width: 100%;" name="" id="">
 				  			<option value="">نوع الزجاج الداخلي</option>
-				  		</select>		  		
+				  		</select>
 				  	</div>
 				  	<div class="col-md-3">
 				  		<label><b>اسم الزجاج الخارجي</b></label>
 				  		<select class="form-control select2" style="width: 100%;" name="" id="">
 				  			<option value="">نوع الزجاج الخارجي</option>
-				  		</select>		  		
-				  	</div>	
+				  		</select>
+				  	</div>
 				  	<div class="col-md-2">
 				  		<label><b>نوع الفراغ</b></label>
 				  		<select class="form-control select2" style="width: 100%;" name="" id="">
 				  			<option value="">نوع الفراغ</option>
 				  			<option value="6">6</option>
 				  			<option value="9">9</option>
-				  		</select>		  		
-				  	</div>		  		  	
+				  		</select>
+				  	</div>
 			  </div>
-			  <br>	
+			  <br>
 			  <div class="row">
 			  	<div class="col-md-1">
 			  		<label><b>العرض</b></label>
 			  		<input type="text" name="" class="form-control">
-			  	</div>	
+			  	</div>
 			  	<div class="col-md-1">
 			  		<label><b>اﻻرتفاع</b></label>
 			  		<input type="text" name="" class="form-control">
-			  	</div>		
+			  	</div>
 			  	<div class="col-md-1">
 			  		<label><b>العدد</b></label>
 			  		<input type="text" name="" class="form-control">
-			  	</div>			  	
+			  	</div>
 			  	<div class="col-md-1">
 			  		<label><b>الداخلي</b></label>
 			  		<input type="text" name="" class="form-control" readonly="">
@@ -95,24 +99,24 @@
 			  	<div class="col-md-1">
 			  		<label><b>الخارجي</b></label>
 			  		<input type="text" name="" class="form-control" readonly="">
-			  	</div>	
+			  	</div>
 			  	<div class="col-md-1">
 			  		<label><b>سعر الفراغ</b></label>
 			  		<input type="text" name="" class="form-control">
-			  	</div>	
+			  	</div>
 			  	<div class="col-md-2">
 			  		<label><b>تكلفة الفراغ</b></label>
 			  		<input type="text" name="" class="form-control" readonly="">
-			  	</div>	
+			  	</div>
 			  	<div class="col-md-2">
 			  		<label><b>سعر القطعة</b></label>
 			  		<input type="text" name="" class="form-control">
-			  	</div>		
+			  	</div>
 			  	<div class="col-md-2">
 			  		<label><b>اﻻجمالي</b></label>
 			  		<input type="text" name="" class="form-control">
-			  	</div>			  		  			  			  		  	
-			  </div>  		
+			  	</div>
+			  </div>
 		  	</form>
 		  	<br>
 		  	<button class="btn btn-success">اضافة</button>
@@ -136,7 +140,22 @@
 		if ($('select[name=glassType]').val() == '')
 		{
 			$('#single').hide();
-			$('#double').hide();			
+			$('#double').hide();
 		}
 	}
+	$("document").ready(function ()
+	{
+		var id = $('input[name=id_pro]').val();
+		$.ajax({
+			type:'ajax',
+			method:'get',
+			url:'<?php echo base_url() ?>Offer/getdatabyid_offer_details',
+			data:{id:id},
+			async:false,
+			dataType:'json',
+			success:function(data){
+				$('#offerDetails_table').html(data);
+			}
+		});
+	})
 </script>
